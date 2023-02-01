d233f9c1b942257571edf1391e6f21917bf2b823
TYNDALL\colum.crowe <colum.crowe@tyndall.ie>
2023-02-01 13:31:24 +0000

    Fix errors

---
+++ launch - ext.cmd
@@ -0,0 +1,34 @@
+@ECHO OFF
+ECHO %cd%
+PAUSE
+pushd \\FS1\Docs3\colum.crowe\My Documents\Python\envs\myenv\Scripts
+ECHO %cd%
+PAUSE
+@ECHO ON
+set PATH=C:\Anaconda3;%PATH%
+set PATH=C:\Anaconda3\Scripts;%PATH%
+set PATH=C:\Anaconda3\Library\bin;%PATH%
+::REM call conda activate myenv
+::REM call conda activate tf1
+set "PYTHONPATH=\\FS1\Docs3\colum.crowe\My Documents\Python\envs\myenv"
+set "VIRTUAL_ENV=\\FS1\Docs3\colum.crowe\My Documents\Python\envs\myenv"
+set PATH=\\FS1\Docs3\colum.crowe\My Documents\Python\envs\myenv\Scripts;%PATH%
+set PATH=\\FS1\Docs3\colum.crowe\My Documents\Python\envs\myenv\Lib;%PATH%
+call activate.bat
+set
+@ECHO OFF
+ECHO %cd%
+PAUSE
+cd ./../
+cd ./../
+cd ./../
+popd
+ECHO %cd%
+PAUSE
+pushd E:\
+ECHO %cd%
+PAUSE
+cd E:\WDVP2023\wdvp2023-interactive\
+ECHO %cd%
+PAUSE
+python -m http.server --directory docs 8008
